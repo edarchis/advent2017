ffc423ec35c09dbd3abd0df9d99dc8f86a21a0be
Eric Darchis <darchis@pobox.com>
2017-12-02 20:20:23 +0100

    Bad refactor, bad.

--- src/day01/part1.kt
+++ src/day01/part1.kt
@@ -3,10 +3,10 @@ package day01
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("1212 -> " + day01b("1212"))
-    println("1221 -> " + day01b("1221"))
-    println("1234 -> " + day01b("1234"))
-    println("91212129 -> " + day01b("91212129"))
+    println("1212 -> " + day01a("1212"))
+    println("1221 -> " + day01a("1221"))
+    println("1234 -> " + day01a("1234"))
+    println("91212129 -> " + day01a("91212129"))
 
     val input: String = File("day01/input.txt").bufferedReader().use { it.readText() }
 
