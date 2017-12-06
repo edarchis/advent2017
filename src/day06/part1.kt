1d7c732f72f8d51e1ad707550fe3370181472592
Eric Darchis <darchis@pobox.com>
2017-12-06 17:39:05 +0100

    Day 06 part 2
    All done from the same method to experiment with the data class to return multiple values.

--- src/day06/part1.kt
+++ src/day06/part1.kt
@@ -2,6 +2,8 @@ package day06
 
 import java.io.File
 
+// This file is doing both part 1 and 2. I merged them on purpose to experiment with the data class to return two values
+
 fun main(args: Array<String>) {
 
     val sample = "0 2 7 0"
@@ -10,17 +12,21 @@ fun main(args: Array<String>) {
 
     val input = File("day06/input.txt")
             .bufferedReader()
-            .use { it.readText() }
+            .readLine()
             .split("\t".toRegex())
             .map { it.toInt() }
             .toIntArray()
     println("input ${input.count()} items gives: " + relocate(input))
 }
 
-fun relocate(banks: IntArray): Int {
+data class Loop(val steps: Int, val cycles: Int)
+fun relocate(banks: IntArray): Loop {
     var steps = 0
     var banksTrail: MutableList<List<Int>> = mutableListOf()
 
+    // I chose the do while approach while my colleague Youri preferred the break to avoid adding a value at the
+    // beginning and comparing to an altered version in the condition. After doing some Groovy, I'm too
+    // happy to use a do while :)
     do {
         banksTrail.add(banks.toList())
         val maxBankIndex = getMaxBankIndex(banks)
@@ -34,10 +40,11 @@ fun relocate(banks: IntArray): Int {
         steps++
     } while (banks.toList() !in banksTrail)
 
-    return steps
+    return Loop(steps, steps-banksTrail.indexOf(banks.toList()))
 }
 
 fun getMaxBankIndex(banks: IntArray) : Int {
+    // This is not the easiest method but it allowed me to try the withIndex()
     return banks.withIndex().maxWith (Comparator { x, y ->
         when {
             x.value == y.value -> y.index - x.index
