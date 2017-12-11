474e3fe86518fbf27ff69f62df846bf53fdd9abb
Eric Darchis <darchis@pobox.com>
2017-12-11 15:38:43 +0100

    Fix for day 04 part 2.
    toSortedSet() worked by luck only: aab & abb were seen as anagrams of each other because "Set"

--- src/day04/part2.kt
+++ src/day04/part2.kt
@@ -6,6 +6,8 @@ fun main(args: Array<String>) {
 
     println("abcde fghij should be valid: " + isValidPassPhrase2("abcde fghij"))
     println("abcde xyz ecdab should NOT be valid: " + isValidPassPhrase2("abcde xyz ecdab"))
+    println("abb xyz aab SHOULD be valid if you count letters properly (not a Set): "
+            + isValidPassPhrase2("abb xyz aab"))
 
     val passPhraseValidity = File("day04/input.txt").bufferedReader()
             .readLines().associateBy({ it }, { isValidPassPhrase2(it) })
@@ -15,6 +17,6 @@ fun main(args: Array<String>) {
 
 fun isValidPassPhrase2(passPhrase: String): Boolean {
     val words = passPhrase.split(" ")
-    val map = words.associateBy({ it.toSortedSet() }, { 1 })
+    val map = words.associateBy({ it.toList().sorted() }, { 1 })
     return map.size == words.size
 }
